57ea42f1afa28644172bd34dc2f1f14e9f5eec5b
NILADRI CHATTERJEE <niladrichatterjee365@gmail.com>
2024-06-24 10:07:35 +0530

    Final upload!

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ const homeRoutes = require('./routers/home');
 const app = express();
 const port = process.env.port || 8080;
 
-mongoos.connect("mongodb://localhost:27017/studentdetails", {useNewUrlParser : true})
+mongoos.connect("mongodb://localhost:27017/studentdetails", { useNewUrlParser: true })
 const db = mongoos.connection;
-db.on('eror',()=>{
+db.on('eror', () => {
     console.log("Err is ");
 })
-db.once('open',()=>{
+db.once('open', () => {
     console.log("Connected");
 })
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
-// body parser 
+// body parser
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
@@ -26,6 +26,6 @@ app.use(bodyParser.json())
 
 
 
-app.use('/' , homeRoutes)
+app.use('/', homeRoutes)
 
 app.listen(port)
